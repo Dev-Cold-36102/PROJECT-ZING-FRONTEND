c83ecbc2001dfe48358b3f66a1a5b3d8e559835b
hoanglv92 <hoanglv92@gmail.com>
2020-05-20 14:26:58 +0700

    update song.ts

---
+++ src/app/app.routing.ts
@@ -0,0 +1,28 @@
+﻿import {Routes, RouterModule} from '@angular/router';
+
+import {AuthGuard} from './JWT-ROLE/_helpers';
+import {Role} from './JWT-ROLE/_models';
+import {IndexComponent} from './index/index.component';
+import {RegisterComponent} from './register/register.component';
+import {SigninComponent} from './signin/signin.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent,
+    canActivate: [AuthGuard]
+
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  // otherwise redirect to home
+  {path: '**', redirectTo: ''}
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UsersService} from '../_service_not_authen/users.service';
+import {Users} from '../_service_not_authen/users';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private usersService: UsersService) {
+  }
+
+  status = '';
+  registerFrom: FormGroup;
+  regexPhone = '^\\+84\\d{9,10}$';
+
+  ngOnInit(): void {
+    this.registerFrom = new FormGroup({
+      email: new FormControl('', [Validators.required,
+        Validators.email]),
+      userName: new FormControl('', [Validators.required,
+        Validators.minLength(6), Validators.maxLength(45)]
+      ),
+      password: new FormControl('', [Validators.required,
+        Validators.minLength(6), Validators.maxLength(45)]),
+      repass: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit(registerFrom: FormGroup) {
+    const users: Users = registerFrom.value;
+    const regex = '^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$';
+    if (users.userName.length < 6 || users.userName.length > 45){
+      alert('Tài khoản có độ dài từ 6 tới 45 kí tự');
+      return false;
+    }
+    if (users.password.length < 6 || users.password.length > 45 ){
+      alert('password có độ dài từ 6 tới 45 kí tự');
+      return false;
+    }
+    if (users.email.length < 6 || users.email.length > 45 || !users.email.match(regex)){
+      alert('email Sai Định Dạng');
+      return false;
+    }
+    if (users.password !== users.repass) {
+      alert('nhap lai mat khau khong dung');
+      return false;
+    } else {
+      this.usersService.saveUser(users);
+      alert('Tạo Tài Khoản Thành Công, Hãy Đăng Nhập');
+    }
+  }
+}
+
