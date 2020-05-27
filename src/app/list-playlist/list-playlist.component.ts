4a4392723d8014199dc5c2b92dede12e0c7b6be1
lam300695 <lam300695@gmail.com>
2020-05-27 08:48:50 +0700

    Sua xoa plat

--- src/app/list-playlist/list-playlist.component.ts
+++ src/app/list-playlist/list-playlist.component.ts
@@ -6,6 +6,8 @@ import {error} from '@angular/compiler/src/util';
 import {Router} from '@angular/router';
 import {AuthenticationService} from '@app/JWT-ROLE/_services';
 import {User} from '@app/JWT-ROLE/_models';
+import { PlayList } from '../_model/PlayList';
+import { PlaylistService } from '../_service_not_authen/playlist.service';
 
 @Component({
   selector: 'app-list-playlist',
@@ -18,7 +20,7 @@ export class ListPlaylistComponent implements OnInit {
   srcImagePlaylist = 'assets/images/playlist/';
   currentUser: User;
 
-  constructor(private httpClient: HttpClient, private router: Router, private authenticationService: AuthenticationService) {
+  constructor(private httpClient: HttpClient, private router: Router, private authenticationService: AuthenticationService ,private playlistService: PlaylistService) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
 
@@ -38,5 +40,43 @@ export class ListPlaylistComponent implements OnInit {
   deletePlaylist(playlist: Playlist) {
     if (confirm('do you want continue')) {
     }
+  }
+  name: string;
+  playList: PlayList = null;
+
+
+
+
+  edit(item: PlayList) {
+    this.playList = item;
+  }
+
+  onUpdate() {
+    this.playlistService.updatePlaylists(this.playList).subscribe(data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  delete(id: number) {
+    this.playlistService.deletePlaylists(id).subscribe(data => {
+          let index = this.getIndex(data.id);
+          this.playLists.splice(index, 1);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  getIndex(id: number): number {
+    let result = 0;
+    this.playLists.forEach((item, index ) => {
+      if (item.id == id) {
+        result = index;
+      }
+    });
+    return result;
   }
 }
